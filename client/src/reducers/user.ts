61f3ff1f38ab6670b6fc5864784b52b035a7de54
Yevhen Piotrovskyi <yevhen.piotrovskyi@akvelon.com>
2017-05-24 15:50:17 +0300

    add initial client

---
+++ client/src/reducers/user.ts
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import { combineEvents } from '../utils/combineEvents';
+import {
+  USER_LOGIN,
+  USER_LOGOUT,
+} from '../constants'
+
+const user = combineEvents({
+  [USER_LOGIN]: (state: any, { res }: any): any => res.user,
+  [USER_LOGOUT]: (): null => null,
+}, null)
+
+export default combineReducers({
+  user,
+})
---
+++ client/src/utils/combineEvents.ts
@@ -0,0 +1,11 @@
+export function combineEvents(reducers: any, initialState: any): any {
+  return (state: any = initialState, event: any) => {
+    if (reducers.hasOwnProperty(event.type)) {
+      return reducers[event.type](state, event);
+    } else if (reducers.hasOwnProperty('default')) {
+      return reducers.default(state, event);
+    } else {
+      return state;
+    }
+  }
+}
