c03850e09d876d481367772769ca65c81ee3ecb1
Valerii Radchenko <valerii.radchenko@akvelon.com>
2017-05-24 14:55:17 +0300

    Added user_id field to Project table

--- migrations/002-patch.sql
+++ migrations/002-patch.sql
@@ -1,7 +1,7 @@
 -- Up
-CREATE TABLE Project (id INTEGER PRIMARY KEY, name);
+CREATE TABLE Project (id INTEGER PRIMARY KEY, name VARCHAR(255), user_id INTEGER);
 
-INSERT INTO Project (id, name) VALUES (1, 'Initial project');
+INSERT INTO Project (id, name, user_id) VALUES (1, 'Initial project', 1);
 
 -- Down
 DROP TABLE Project;
