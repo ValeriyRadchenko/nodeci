ad36b076a0a4fe9b106ab2a261f61be3e6b749c5
Valerii Radchenko <valerii.radchenko@akvelon.com>
2017-05-24 16:18:43 +0300

    Added Koa static server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as serve from 'koa-static';
 import { ProjectDB } from './db/project-db';
+import * as path from 'path';
 
 const normalizePort = require('normalize-port');
 const port = normalizePort(process.env.PORT || '3000');
@@ -8,15 +10,18 @@ const port = normalizePort(process.env.PORT || '3000');
 const app = new Koa();
 const router = new Router();
 
-router.get('/', async (ctx: any, next: any) => {
+router.get('/project', async (ctx: any, next: any) => {
     let projectDB = new ProjectDB();
     ctx.body = await projectDB.getAll();
     await next();
 });
 
+// app.use(serve(path.resolve(__dirname, '..', '..', 'client', 'dist')));
+app.use(serve('./client/dist'));
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(port, async () => {
+app.listen(port, () => {
     console.log(`Server listen port ${port}`);
 });
