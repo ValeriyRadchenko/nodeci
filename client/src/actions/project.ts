b00dbebb51cd56e5e2e33d09272f6aaa4bdb041a
Yevhen Piotrovskyi <yevhen.piotrovskyi@akvelon.com>
2017-05-25 17:10:10 +0300

    fix initial loading

--- client/src/actions/project.ts
+++ client/src/actions/project.ts
@@ -1,5 +1,7 @@
 import {
-    PROJECT_SET_LIST
+    PROJECT_SET_LIST,
+    PROJECT_GET_LIST,
 } from '../constants';
 
 export const setProjectsList = (projects: Array<object>) => ({ type: PROJECT_SET_LIST, projects });
+export const getProjectsList = () => ({ type: PROJECT_GET_LIST });
--- client/src/saga/user.ts
+++ client/src/saga/user.ts
@@ -1,7 +1,8 @@
 import { takeEvery, put as dispatch } from 'redux-saga/effects';
 import { userService } from '../api';
 import {
-    setProjectsList
+    setProjectsList,
+    getProjectsList,
 } from '../actions/project';
 import {
     setUserToken,
@@ -12,8 +13,9 @@ import {
 } from '../constants';
 import { push } from 'react-router-redux';
 
-function * initial(): any {
+function * initial(token: string): any {
     // load current user meta
+    yield dispatch(getProjectsList());
 }
 
 function * userLogin(action: any): any {
@@ -22,6 +24,7 @@ function * userLogin(action: any): any {
   if (success) {
     yield dispatch(setUserToken(payload.token));
     localStorage.setItem('token', payload.token);
+    yield initial(payload.token);
     yield dispatch(push('/'));
   }
 }
@@ -32,7 +35,10 @@ function * userLogout(action: any): any {
 }
 
 export default function * userSaga() {
-    yield initial();
+    const token = localStorage.getItem('token');
+    if (token) {
+      yield initial(token);
+    }
 
     yield * [
         takeEvery(USER_LOGIN, userLogin),
