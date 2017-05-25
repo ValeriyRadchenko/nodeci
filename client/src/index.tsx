9482c356f0dc3fe1b245c1746e47ebc2091395a8
Yevhen Piotrovskyi <yevhen.piotrovskyi@akvelon.com>
2017-05-25 13:46:56 +0300

    replace history

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-const { Router, Route, browserHistory } = require('react-router');
+const { Router, Route, hashHistory } = require('react-router');
 import { Provider } from 'react-redux';
 const { syncHistoryWithStore } = require('react-router-redux');
 import store from './store';
 import Layout from './containers/Layout';
 import Projects from './containers/Projects';
+import NotFound from './components/NotFound';
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
 const routes = (
   <Provider store={store}>
     <Router history={history}>
       <Route component={Layout}>
-        <Route path="/" component={Projects} />
+        <Route path='/' component={Projects} />
+        <Route path='*' component={NotFound} />
       </Route>
     </Router>
   </Provider>
-)
+);
 
 render(routes, document.getElementById('root'));
