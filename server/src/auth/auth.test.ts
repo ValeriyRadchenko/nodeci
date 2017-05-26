0ad663e8f175573118e0e071fade3d55b0ef579c
Valerii Radchenko <valerii.radchenko@akvelon.com>
2017-05-26 08:50:59 +0300

    should decode token test

--- server/src/auth/auth.test.ts
+++ server/src/auth/auth.test.ts
@@ -2,6 +2,7 @@ import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import { auth } from './auth';
 import chaiSubset = require('chai-subset');
+import { UnauthorizedError } from '../errors/errors';
 
 chai.use(chaiSubset);
 chai.use(chaiAsPromised);
@@ -12,5 +13,11 @@ describe('auth service test', () => {
     it('should return string token', async () => {
         let token = await auth.sign({uid: 1});
         expect(typeof token === 'string').to.be.true;
+    });
+
+    it ('should decode token', async () => {
+        let token = await auth.sign({uid: 1});
+        let decoded = await auth.verify(token);
+        expect(decoded.uid === 1).to.be.true;
     });
 });
